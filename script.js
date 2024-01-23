054b0e851575fc442c8a3f46fbd5d3618418b444
Danny Moulier <mr.moulier@gmail.com>
2024-01-24 01:14:09 +0200

    validation improved

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function validatePassword() {
-    let pass1 = document.querySelector('#password1')
-    let pass2 = document.querySelector('#password2')
-    let errorMessage = document.querySelector('.error')
+  let pass1 = document.querySelector("#password1");
+  let pass2 = document.querySelector("#password2");
+  let errorInputs = document.querySelectorAll(".error");
+  let errorMessage = document.querySelector(".error-message");
 
-    if (pass1 !== pass2){
-        errorMessage.innerText = 'passwords not match'
-    } else if (pass1 === pass2) {
-        errorMessage.innerText = ''
-    }
+  if (pass1.value !== pass2.value) {
+    errorInputs.forEach((errorInput) => {
+      errorInput.style.border = "solid rgb(198, 0, 0)";
+    });
+    errorMessage.innerText = "Passwords do not match"
+    return false;
+  }
+  return true;
 }
-
-// validatePassword()
