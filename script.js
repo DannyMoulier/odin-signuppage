f110ad05c0d071ca19a689aab841afd18d151e27
Danny Moulier <mr.moulier@gmail.com>
2024-01-30 19:22:54 +0200

    minor fixes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function validatePassword() {
-  let pass1 = document.querySelector("#password1");
-  let pass2 = document.querySelector("#password2");
-  let errorInputs = document.querySelectorAll(".error");
-  let errorMessage = document.querySelector(".error-message");
+  const pass1 = document.querySelector("#password1");
+  const pass2 = document.querySelector("#password2");
+  const errorInputs = document.querySelectorAll(".error");
+  const errorMessage = document.querySelector(".error-message");
 
   if (pass1.value !== pass2.value) {
     errorInputs.forEach((errorInput) => {
       errorInput.style.border = "1.5px solid rgb(198, 0, 0)";
     });
-    errorMessage.innerText = "Passwords do not match"
+    errorMessage.innerText = "Passwords do not match";
     return false;
+  } else {
+    errorInputs.forEach((errorInput) => {
+      errorInput.style.borderColor = "var(--color-base-input-border)";
+    });
+    errorMessage.innerText = "";
+    return true;
   }
-  return true;
 }
